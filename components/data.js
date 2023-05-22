5f05548813a167705c2cef5cb72fd7d1571d012a
omowonuola <oyedmola@gmail.com>
2023-05-22 03:56:05 +0100

    waitlist landing page setup

---
+++ components/data.js
@@ -0,0 +1,60 @@
+import {
+  FaceSmileIcon,
+  ChartBarSquareIcon,
+  CursorArrowRaysIcon,
+  DevicePhoneMobileIcon,
+  AdjustmentsHorizontalIcon,
+  SunIcon,
+} from "@heroicons/react/24/solid";
+
+import benefitOneImg from "../public/img/benefit-one.png";
+import benefitTwoImg from "../public/img/benefit-two.png";
+
+const benefitOne = {
+  title: "Seamless Insurance Process",
+  desc: "It eliminates the need for lengthy paperwork, phone calls, and multiple visits to insurance providers. It allows for quick and efficient completion of insurance-related tasks, saving valuable time for vehicle owners.",
+  image: benefitOneImg,
+  bullets: [
+    {
+      title: "Convenience",
+      desc: "With seamlessclaims process, vehicle owners can handle their insurance needs through our apps, providing convenience and accessibility. They can easily compare quotes, make policy changes, submit claims, and access policy documents at their convenience.",
+      icon: <FaceSmileIcon />,
+    },
+    {
+      title: "Enhanced customer experience",
+      desc: "seamlessclaims process provides a smooth and hassle-free experience for customers. It reduces the frustration and stress often associated with complex insurance procedures. This, in turn, helps to improved our customer satisfaction and loyalty.",
+      icon: <ChartBarSquareIcon />,
+    },
+    {
+      title: "Faster claims processing",
+      desc: "With seamlessclaims process, claims can be submitted online, accompanied by relevant documents and evidence. This expedites the claims handling process, leading to quicker resolution and reimbursement for vehicle owners.",
+      icon: <CursorArrowRaysIcon />,
+    },
+  ],
+};
+
+const benefitTwo = {
+  title: "More Benefits",
+  // desc: "You can use this same layout with a flip image to highlight your rest of the benefits of your product. It can also contain an image or Illustration as above section along with some bullet points.",
+  image: benefitTwoImg,
+  bullets: [
+    {
+      title: "Streamlined communication",
+      desc: "Seamlessclaims processes involves using our digital platforms that facilitate effective communication between insurance providers and customers. It allows for timely updates, instant notifications, and efficient resolution of queries or concerns.",
+      icon: <DevicePhoneMobileIcon />,
+    },
+    {
+      title: "Cost-effectiveness",
+      desc: "SeamlessClaims insurance process allows vehicle owners to easily compare quotes from different providers, enabling them to find the most competitive rates and coverage options. This promotes cost-effectiveness and helps in making informed decisions based on individual needs and budgets.",
+      icon: <AdjustmentsHorizontalIcon />,
+    },
+    {
+      title: "Dark & Light Mode",
+      desc: "Nextly comes with a zero-config light & dark mode. ",
+      icon: <SunIcon />,
+    },
+  ],
+};
+
+
+export {benefitOne, benefitTwo};
---
+++ components/hero.js
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import Container from "./container";
+import heroImg from "../public/img/hero.png";
+
+const Hero = () => {
+  return (
+    <>
+      <Container className="flex flex-wrap ">
+        <div className="flex items-center w-full lg:w-1/2">
+          <div className="max-w-2xl mb-8">
+            <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
+              COMING SOON !!!
+            </h1>
+            <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
+              Seamlessclaims is an online marketplace that connects individuals and businesses with insurance providers. 
+              It provides an opportunity to compare rates, buy insurance directly from insurance companies, 
+              and allows for seamless management of the portfolio
+            </p>
+
+            <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
+              {/* <a
+                href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
+                target="_blank"
+                rel="noopener"
+                className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
+                Download for Free
+              </a> */}
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-center w-full lg:w-1/2">
+          <div className="">
+            <Image
+              src={heroImg}
+              width="616"
+              height="617"
+              className={"object-cover"}
+              alt="Hero Illustration"
+              loading="eager"
+              placeholder="blur"
+            />
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+}
+
+
+export default Hero;
